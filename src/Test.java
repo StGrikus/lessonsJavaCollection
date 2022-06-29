d0a7a62131b2ecb8323f58fea732d6703ba1bf5b
Grikus <saintgrikus@gmail.com>
2022-06-29 21:38:04 +0300

    firstcommit

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> animals = new ArrayList<>();
+
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("frog");
+        animals.add("bird");
+
+        Collections.sort(animals);
+        System.out.println(animals);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(0);
+        numbers.add(500);
+        numbers.add(100);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+}
